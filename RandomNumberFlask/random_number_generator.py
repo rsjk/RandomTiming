bbfe0cfcdd396e22b2d2a1d4f7f26f32e4e0498d
Rachel Selby <rachel.b.selby-1@ou.edu>
2019-09-13 18:37:45 -0500

    remove venv

--- RandomNumberFlask/random_number_generator.py
+++ RandomNumberFlask/random_number_generator.py
@@ -1,16 +1,22 @@
 from flask import Flask
 import random
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Home'
 
+
 @app.route('/RandomNumberGenerator')
 def random_number_generator():
-    num = random.randint(1, 1000001)
+    # generate a number between 1 and 1 million
+    num = random.randint(1, 1000000)
+    # must return a string
     return str(num)
+
 
 if __name__ == '__main__':
     app.run()
