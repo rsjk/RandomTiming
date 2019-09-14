f2e0d0aff91759418759e42bb17d7d4fbe6de511
Andy-Vu-Viz <43096097+Andy-Vu-Viz@users.noreply.github.com>
2019-09-14 13:54:49 -0500

    Update and rename testscript-Java-AppEngine.py to testscript.py

--- testscript.py
+++ testscript.py
@@ -1,12 +1,13 @@
 import re
 import urllib.request
+import math
 from bs4 import BeautifulSoup
 
 numbers = []
-nonuniques = 0
 fails = 0
 logfile = open("test_log.txt","w+")
 for f in range(10):
+    nonuniques = 0
     for k in range(1000):
         html = urllib.request.urlopen('https://randomnumbergeneratorclean.appspot.com/')
         soup = BeautifulSoup(html, features="html.parser")
@@ -22,7 +23,9 @@ for f in range(10):
             if j == l:
                 ++nonuniques
                 print('Nonuniques: ', nonuniques)
-    nonuniques = nonuniques/2
+    nonuniques = math.ceil(nonuniques/2)
     if nonuniques > 250:
         ++fails
-logfile.write("Java App Engine Fails: ", fails)
+logfile.write("750/1000 Unique Test Fails: ")
+logfile.write(str(fails))
+logfile.close()
