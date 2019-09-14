c89a1a139497aaa67fbc8562a3a801b54d94fdca
Rachel Selby <rachel.b.selby-1@ou.edu>
2019-09-14 16:56:31 -0500

    fix test script and add results

--- testscript.py
+++ testscript.py
@@ -2,30 +2,60 @@ import re
 import urllib.request
 import math
 from bs4 import BeautifulSoup
+import sys
 
-numbers = []
-fails = 0
-logfile = open("test_log.txt","w+")
-for f in range(10):
-    nonuniques = 0
-    for k in range(1000):
-        html = urllib.request.urlopen('https://randomnumbergeneratorclean.appspot.com/')
-        soup = BeautifulSoup(html, features="html.parser")
-        data = soup.findAll(text=True)
+
+# Generate 1000 random numbers 10 times and check that there are at least 750 unique numbers in each iteration
+def get_random_numbers(url):
+    passes = 0
+    fails = 0
+    logfile = open("test_log.txt","w+")
+    for f in range(0, 10):
+        numbers = []
+        for k in range(0, 1000):
+            # Request from the given url
+            html = urllib.request.urlopen(url)
+
+            # Parse the text from the html
+            soup = BeautifulSoup(html, features="html.parser")
+            data = soup.findAll(text=True)
          
-        for i in list(data):
-            if i.isdigit():
-                print(k,': ',i)
-                numbers.append(i)
+            # Print the numbers to the command line
+            for i in list(data):
+                if i.isdigit():
+                    print(k,': ',i)
+                    numbers.append(i)
 
-    for j in range(len(numbers)):
-        for l in range(len(numbers)):
-            if j == l:
-                ++nonuniques
-                print('Nonuniques: ', nonuniques)
-    nonuniques = math.ceil(nonuniques/2)
-    if nonuniques > 250:
-        ++fails
-logfile.write("750/1000 Unique Test Fails: ")
-logfile.write(str(fails))
-logfile.close()
+        # Make a set of the numbers -- sets can only contain unique elements, so we can check for duplicates this way
+        numbers_set = set(numbers)
+
+        header = "Run " + str(f + 1) + ":\n"
+        logfile.write(header)
+
+        # Check if there are at least 750 unique numbers
+        if len(numbers_set) < 750:
+            logfile.write("Test failed\n")
+            fails += 1
+        else:
+            logfile.write('Test passed\n')
+            passes += 1
+        logfile.write(str(len(numbers_set)))
+        logfile.write(" unqiue numbers were retrieved\n")
+
+    # Log the total passes and fails
+    total_passes_str = "Total passes: " + str(passes) + "\n"
+    logfile.write(total_passes_str)
+    total_fails_str = "Total fails: " + str(fails) + "\n"
+    logfile.write(total_fails_str)
+
+    logfile.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide a URL")
+    else:
+        # Get the url from the second arg
+        url = sys.argv[1]
+        # Get the random numbers
+        get_random_numbers(url)
