8ed8c33fd82020516ae81249701e874da2aa969d
Andy-Vu-Viz <43096097+Andy-Vu-Viz@users.noreply.github.com>
2019-09-14 13:49:53 -0500

    Add files via upload

--- testscript-Java-AppEngine.py
+++ testscript-Java-AppEngine.py
@@ -4,26 +4,25 @@ from bs4 import BeautifulSoup
 
 numbers = []
 nonuniques = 0
-for k in range(1000):
-    html = urllib.request.urlopen('https://randomnumbergeneratorclean.appspot.com')
-    soup = BeautifulSoup(html, features="html.parser")
-    data = soup.findAll(text=True)
-     
-    def visible(element):
-        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-            return False
-        elif re.match('<!--.*-->', str(element.encode('utf-8'))):
-            return False
-        return True
-     
-    result = filter(visible, data)
-    for i in list(result):
-        if i.isdigit():
-            print(k,': ',i)
-            numbers.append(i)
+fails = 0
+logfile = open("test_log.txt","w+")
+for f in range(10):
+    for k in range(1000):
+        html = urllib.request.urlopen('https://randomnumbergeneratorclean.appspot.com/')
+        soup = BeautifulSoup(html, features="html.parser")
+        data = soup.findAll(text=True)
+         
+        for i in list(data):
+            if i.isdigit():
+                print(k,': ',i)
+                numbers.append(i)
 
-for j in range(len(numbers)):
-    for l in range(len(numbers)):
-        if j == l:
-            ++nonuniques
-            print('Nonuniques: ', nonuniques)
+    for j in range(len(numbers)):
+        for l in range(len(numbers)):
+            if j == l:
+                ++nonuniques
+                print('Nonuniques: ', nonuniques)
+    nonuniques = nonuniques/2
+    if nonuniques > 250:
+        ++fails
+logfile.write("Java App Engine Fails: ", fails)
