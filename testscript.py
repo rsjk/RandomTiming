559a695fb60361a0014bb716cb4964bfee0804e6
LordYako <blakerobertson808@gmail.com>
2019-09-19 02:13:00 -0500

    rewrote testscript.py for ping test

--- testscript.py
+++ testscript.py
@@ -3,50 +3,28 @@ import urllib.request
 import math
 from bs4 import BeautifulSoup
 import sys
+import time
 
-
-# Generate 1000 random numbers 10 times and check that there are at least 750 unique numbers in each iteration
+# Tests ping of rendom number generators
 def get_random_numbers(url):
-    passes = 0
-    fails = 0
     logfile = open("test_log.txt","w+")
-    for f in range(0, 10):
-        numbers = []
-        for k in range(0, 1000):
-            # Request from the given url
-            html = urllib.request.urlopen(url)
 
-            # Parse the text from the html
-            soup = BeautifulSoup(html, features="html.parser")
-            data = soup.findAll(text=True)
-         
-            # Print the numbers to the command line
-            for i in list(data):
-                if i.isdigit():
-                    print(k,': ',i)
-                    numbers.append(i)
+    pingstart = time.time()
+   ping start = time.time()
+    html = urllib.request.urlopen(url)
+    pingend = time.time()
+    totalping = pingend - pingstart
+    # Parse the text from the html
+    soup = BeautifulSoup(html, features="html.parser")
+    data = soup.findAll(text=True)
 
-        # Make a set of the numbers -- sets can only contain unique elements, so we can check for duplicates this way
-        numbers_set = set(numbers)
-
-        header = "Run " + str(f + 1) + ":\n"
-        logfile.write(header)
-
-        # Check if there are at least 750 unique numbers
-        if len(numbers_set) < 750:
-            logfile.write("Test failed\n")
-            fails += 1
-        else:
-            logfile.write('Test passed\n')
-            passes += 1
-        logfile.write(str(len(numbers_set)))
-        logfile.write(" unqiue numbers were retrieved\n")
-
+    # Print the numbers to the command line
+    for i in list(data):
+       if i.isdigit():
+          print(k,': ',i)
     # Log the total passes and fails
-    total_passes_str = "Total passes: " + str(passes) + "\n"
-    logfile.write(total_passes_str)
-    total_fails_str = "Total fails: " + str(fails) + "\n"
-    logfile.write(total_fails_str)
+    t = "Total time: " + str(totalping) + "\n"
+    logfile.write(totalping)
 
     logfile.close()
 
